9ec49394104262cb0b04b98115d633f21c73cc8f
QuantumWorkMechanics <ihrydziu@quantumwork.com>
2023-09-29 10:24:54 -0400

    Update Mobile style

--- src/components/Questionnaire/Questionnaire.jsx
+++ src/components/Questionnaire/Questionnaire.jsx
@@ -77,19 +77,19 @@ function Questionnaire() {
           <h1 className="pt-2 pl-2 md:p-6 md:pl-40 text-xl md:text-3xl font-bold">
             My Assessment
           </h1>
-          <div className="divider w-40 ml-6 md:ml-40"></div>
+          <div className=" divider w-40 ml-6 md:ml-40"></div>
           <h2 className="text-lg md:text-xl font-light pl-8 md:pl-0 md:ml-40 md:pl-2 -mt-5 md:mt-0 bg-slate-100 md:bg-white">
             {questionNum.Type}
           </h2>
         </header>
         <div className="md:ml-40 md:bg-[#bde1f7] w-fit p-4">
-          <div className="-mt-2 md:mt-2  md:-mb-1 md:ml-4 font-bold text-slate-600 text-sm md:text-md">
+          <div className="md:mt-2  md:-mb-1 md:ml-4 font-bold text-slate-600 text-sm md:text-md">
             Question {questionNum.Number} of {questionNum.Of}
           </div>
           <h3 className=" font-bold text-lg md:text-2xl">
             {questionNum.Dimension}
           </h3>
-          <div className="md:hidden divider"></div>
+          {/* <div className="md:hidden divider"></div> */}
         </div>
         <div className="w-1/4 hidden md:flex text-xs md:-ml-5 md:-mb-5 mt-3 font-bold">
           <div className="ml-40 flex items-center pl-5 py-4">
@@ -101,7 +101,7 @@ function Questionnaire() {
             Desired
           </div>
         </div>
-        <div className="md:hidden p-1 relative mb-2 px-4 mb-8 ">
+        <div className="md:hidden p-1 relative mb-2 px-4 mb-8 mt-4">
           <div className="absolute -mt-5 text-sm">
             {currentSlide == 1 && questionNum.Response_1}
             {currentSlide == 2 && questionNum.Response_2}
